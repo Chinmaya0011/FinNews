eff922d286ad39fafb5f6cdd0eff1dd1e8cd3948
Chinmaya0011 <103204520+Chinmaya0011@users.noreply.github.com>
2024-01-01 11:12:20 +0530

    Update news.js

--- news.js
+++ news.js
@@ -1,5 +1,6 @@
 function fetchNewsByQuery(query) {
-    const url = `https://newsapi.org/v2/everything?q=${query}&from=2023-11-30&sortBy=publishedAt&apiKey=6121f568485746fd89d05bac2433d94b`;
+    const url = `
+    https://newsapi.org/v2/everything?q=${query}&from=2023-12-01&sortBy=publishedAt&apiKey=6121f568485746fd89d05bac2433d94b`;
 
     fetch(url)
         .then(response => response.json())
@@ -7,21 +8,27 @@ function fetchNewsByQuery(query) {
             // Clear existing articles
             const articlesContainer = document.getElementById("articles-container");
             articlesContainer.innerHTML = '';
-            const limitedArticles = data.articles.slice(0, 25);
-            // Append new articles
-            limitedArticles.forEach(article => {
-                articlesContainer.innerHTML += `
-                <div class="article">
-                    <div class="articleContainer">
-                        <img class="articleImage" src="${article.urlToImage}" alt="">
-                        <h1 class="articleTitle">${article.title}</h1>
-                        <p class="articleDescription">${article.description}</p>
-                        <p class="articleAuthor">${article.author}</p>
-                        <p class="articlePublished">${article.publishedAt}</p>
-                        <a href="${article.url}" target="_blank" class="readMoreButton">Read more</a>
-                    </div>
-                </div>`;
-            });
+
+            // Check if data.articles is defined and is an array
+            if (data.articles && Array.isArray(data.articles)) {
+                const limitedArticles = data.articles.slice(0, 25);
+                // Append new articles
+                limitedArticles.forEach(article => {
+                    articlesContainer.innerHTML += `
+                    <div class="article">
+                        <div class="articleContainer">
+                            <img class="articleImage" src="${article.urlToImage}" alt="">
+                            <h1 class="articleTitle">${article.title}</h1>
+                            <p class="articleDescription">${article.description}</p>
+                            <p class="articleAuthor">${article.author}</p>
+                            <p class="articlePublished">${article.publishedAt}</p>
+                            <a href="${article.url}" target="_blank" class="readMoreButton">Read more</a>
+                        </div>
+                    </div>`;
+                });
+            } else {
+                console.error('Invalid data format:', data);
+            }
         })
         .catch(error => console.error('Error fetching data:', error));
 }
@@ -35,4 +42,4 @@ function searchInput() {
 function fetchNews(category) {
     fetchNewsByQuery(category.toLowerCase());
 }
-fetchNewsByQuery('bihar')
+
